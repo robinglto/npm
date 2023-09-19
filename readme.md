8fbf5487720ccd8cd0b7e8820b469f37b2d03db2
robinglto <robinsomedia@pm.me>
2023-09-19 00:10:28 -0400

    Initial commit

---
+++ readme.md
@@ -0,0 +1,1 @@
+A library that adds, subtracts, and multiplies created to test npm.
---
+++ src/index.js
@@ -0,0 +1,3 @@
+export var add = function (a, b) { return a + b; };
+export var subtract = function (a, b) { return a - b; };
+export var multiply = function (a, b) { return a * b; };
